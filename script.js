f7d9e730be5213487e29e60223647808ab974516
Oaksoe11 <osk@sfu.ca>
2025-02-08 20:57:18 -0800

    Add JavaScript Functionality to handle button click events

--- script.js
+++ script.js
@@ -11,39 +11,50 @@ function getComputerChoice(){
     
 }
 
-function getHumanChoice(){
-    let humanChoice = prompt("Please enter Rock or Paper or Scissors: ");
-    while ( !humanChoice || !["rock", "paper","scissors"].includes(humanChoice.toLocaleLowerCase()) ) {
-        humanChoice = prompt("Invalid input. Please enter Rock or Paper or Scissors: ");
-    }
-    return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-}
+// function getHumanChoice(){
+//     let humanChoice = prompt("Please enter Rock or Paper or Scissors: ");
+//     while ( !humanChoice || !["rock", "paper","scissors"].includes(humanChoice.toLocaleLowerCase()) ) {
+//         humanChoice = prompt("Invalid input. Please enter Rock or Paper or Scissors: ");
+//     }
+//     return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+// }
 
 let humanScore = 0;
 let computerScore = 0;
+const messageBox = document.getElementById("message");
 
-function playRound(computerSelection, humanSelection ) {
+function playRound(humanSelection ) {
+    const computerSelection = getComputerChoice();
+
     console.log(`Your choice is ${humanSelection}`);
-    console.log(`Computer choice is ${computerSelection}`);
+    console.log(`Computer choice is ${computerSelection}`)
 
     if ( computerSelection == humanSelection ){
-        console.log("It is a tie!");
+        message.textContent = "It is a tie!";
     }else if ( computerSelection == "Rock" && humanSelection == "Scissors" ||
                computerSelection == "Scissors" && humanSelection == "Paper" ||
                computerSelection == "Paper" && humanSelection == "Rock" 
             ){
-               console.log("Computer wins!");
-               computerScore++;
+                message.textContent = "Computer Wins!";
+                computerScore++;
 
     }else if (  humanSelection == "Rock" && computerSelection == "Scissors" ||
                 humanSelection == "Scissors" && computerSelection == "Paper" ||
                 humanSelection == "Paper" && computerSelection == "Rock" 
             ){
-                console.log("You win!");
+                message.textContent = "You win!";
                 humanScore++;
             }
+    
 }
 
 
+const rock = document.getElementById("Rock");
+const paper = document.getElementById("Paper");
+const scissors = document.getElementById("Scissors");
 
-playGame();
+rock.addEventListener("click", () => {playRound("Rock");});
+paper.addEventListener("click", () => {playRound("Paper");});
+scissors.addEventListener("click", () => {playRound("Scissors");});
+
+
